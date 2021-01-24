fd069912092edcc19f807644437f7c7e4aadca65
Igor Maliy <imaliy.filsh@gmail.com>
2021-01-25 00:41:06 +0200

    Development

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -46,8 +46,7 @@ class DefaultController
     ) {
         $this->viewRenderer = $viewRenderer
             ->withController($this)
-            ->withViewBasePath(dirname(dirname(__DIR__)) . '/views')
-            ->withCsrf();
+            ->withViewBasePath(dirname(dirname(__DIR__)) . '/views');
 
         $this->responseFactory = $responseFactory;
         $this->templateRepo = $templateRepo->withBrand($brandLocator->getBrand());
